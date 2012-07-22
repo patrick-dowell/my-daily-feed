1409d6a9feec0faf6523ddccbe32563b54bac52a
Chris Alexander <chris@fahrenheit.com>
2012-07-22 12:16:25 -0700

    changed name to bold

--- mysite/test_app/views.py
+++ mysite/test_app/views.py
@@ -2,4 +2,4 @@ from test_app.lib.jinja2django import render_to_response
 # Create your views here.
 
 def index(request):
-    return render_to_response("index.html", context={"name": "foo"})
+    return render_to_response("index.html", context={"name": "chris"})
