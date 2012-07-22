dab82cc113b6f51598bdf29601fcf158e884cab4
Chris Alexander <chris@fahrenheit.com>
2012-07-22 12:44:28 -0700

    changed name to "joe"

--- mysite/test_app/views.py
+++ mysite/test_app/views.py
@@ -2,4 +2,4 @@ from test_app.lib.jinja2django import render_to_response
 # Create your views here.
 
 def index(request):
-    return render_to_response("index.html", context={"name": "bob"})
+    return render_to_response("index.html", context={"name": "joe"})
