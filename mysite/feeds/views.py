e914b4558d221b6e36215119ef154ac955877fd9
patrick-dowell <patrick.dowell@gmail.com>
2012-09-09 14:11:05 -0400

    updated view for feeds

--- mysite/feeds/views.py
+++ mysite/feeds/views.py
@@ -4,5 +4,13 @@ from feeds.models import FeedItem
 
 def index(request):
     latest_feed_item_list = FeedItem.objects.all().order_by('-created_at')
-    context = { "feed_items": [{"title": x.title, "url": x.url, "created_at": x.created_at} for x in latest_feed_item_list ]}
+    context = { "feed_items": [{"title": x.title, 
+                                "url": x.url, 
+                                "created_at": x.created_at,
+                                "domain": x.domain,
+                                "source_date": x.source_date,
+                                "photo_link": x.photo_link,
+                                "byline": x.byline,
+                                "excerpt": x.excerpt} 
+                                for x in latest_feed_item_list ]}
     return render_to_response("feed.html", context=context)
