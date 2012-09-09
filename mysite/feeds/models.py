8b7033eb8c72b98f7cf7302203907db164cf3569
patrick-dowell <patrick.dowell@gmail.com>
2012-09-09 13:42:53 -0400

    Enhancing our feed model

--- mysite/feeds/models.py
+++ mysite/feeds/models.py
@@ -5,4 +5,9 @@ from django.db import models
 class FeedItem(models.Model):
     title = models.CharField(max_length=200, null=False)
     url = models.CharField(max_length=200, null=True)
+    domain = models.CharField(max_length=200, null=False)
     created_at = models.DateTimeField(null=False)
+    source_date = models.DateTimeField(null=True)
+    photo_link = models.CharField(max_length=200, null=True)
+    byline = models.CharField(max_length=200, null=True)
+    excerpt = models.CharField(max_length=200, null=False)
