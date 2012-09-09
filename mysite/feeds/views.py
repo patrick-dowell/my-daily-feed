5c91105ee27ab542d7f928cacf84df89d24e16ae
patrick-dowell <patrick.dowell@gmail.com>
2012-09-09 15:18:11 -0400

    adding nones

--- mysite/feeds/views.py
+++ mysite/feeds/views.py
@@ -9,8 +9,8 @@ def index(request):
                                 "created_at": x.created_at.strftime('%B %d, %Y - %I:%M %p'),
                                 "domain": x.domain,
                                 "source_date": x.source_date.strftime('%B %d, %Y - %I:%M %p'),
-                                "photo_link": x.photo_link,
-                                "byline": x.byline,
+                                "photo_link": (x.photo_link if (len(x.photo_link) > 2) else None),
+                                "byline": (x.byline if (len(x.byline) > 2) else None),
                                 "excerpt": x.excerpt} 
                                 for x in latest_feed_item_list ]}
     return render_to_response("feed.html", context=context)
